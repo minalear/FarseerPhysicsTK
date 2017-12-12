fc54e0b9d3e5a3764db38761250c0b041eb7f309
Trevor Fisher <minalear@gmail.com>
2017-12-12 10:20:40 -0600

    Added a test project with the original Farseer physics to compare results.

---
+++ FarseerTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FarseerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var game = new FarseerTest())
+            {
+                game.Run();
+            }
+        }
+    }
+}
